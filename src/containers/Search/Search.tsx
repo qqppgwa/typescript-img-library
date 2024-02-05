35f2666fe5937ee9d64e598ebc3b7f4cc00d6b42
qqppgwa <qqppgwa@gmail.com>
2024-02-05 17:43:09 +0800

    feat: 🎸 Add query string to url

--- src/containers/Search/Search.tsx
+++ src/containers/Search/Search.tsx
@@ -1,11 +1,19 @@
-import { type ChangeEvent, type KeyboardEvent, useState, useRef } from 'react';
+import {
+  type ChangeEvent,
+  type KeyboardEvent,
+  useState,
+  useRef,
+  useEffect,
+} from 'react';
+import { useSearchParams } from 'react-router-dom';
 import { fetchSearchGifs } from '../../apis';
 import GifsResultsList from '../../components/GifsResultsList';
 import { SearchWrapper, ListWrapper } from './Styled';
 
 const Search = () => {
-  const [tempKeyword, setTempKeyword] = useState('');
-  const [usedKeyword, setUsedKeyword] = useState('');
+  let [searchParams, setSearchParams] = useSearchParams();
+  const [tempKeyword, setTempKeyword] = useState<string>('');
+  const [usedKeyword, setUsedKeyword] = useState<null | string>('');
   const listBoxRef = useRef<HTMLDivElement>(null);
 
   const handleInputChange = (e: ChangeEvent) => {
@@ -14,9 +22,24 @@ const Search = () => {
   };
 
   const handleKeyDownEnter = (e: KeyboardEvent) => {
-    // 按下 "enter" 才會出現搜尋結果
-    if (e.key === 'Enter') setUsedKeyword(tempKeyword);
+    /** Press "enter" :
+     * 1. update searchParams (add searchParams to URL)
+     * 2. trigger `usedKeyword` updated then do  search
+     * */
+    if (e.key === 'Enter') setSearchParams({ keyword: tempKeyword });
   };
+
+  useEffect(() => {
+    // if input is empty, do nothing
+    if (!searchParams.get('keyword')) {
+      return;
+    }
+
+    const key = searchParams.get('keyword');
+    setTempKeyword(key!);
+    setUsedKeyword(key);
+  }, [searchParams]);
+
   return (
     <>
       <SearchWrapper
