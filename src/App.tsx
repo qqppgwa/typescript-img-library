aeae2cc3382a99aee4b4af5a56e687299cf505e1
qqppgwa <qqppgwa@gmail.com>
2024-02-20 15:48:14 +0800

    feat: 🎸 Set Search as default page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import Main from './containers/Main';
 
 function App() {
+  const path = useLocation().pathname;
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (path === '/') {
+      navigate('/search');
+    }
+  }, [path, navigate]);
   return <Main />;
 }
 
